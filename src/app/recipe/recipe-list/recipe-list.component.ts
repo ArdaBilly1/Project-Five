60bf73666dd465deaaff6371aa8ae94554574da0
ArdaBilly1 <44688849+ArdaBilly1@users.noreply.github.com>
2018-11-17 00:40:32 +0700

    Add files via upload
    Source code dari project five

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import {Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Router,ActivatedRoute } from '@angular/router';
+
+
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+ // @Output() recipeWasSelected=new EventEmitter<Recipe>();
+  recipes:Recipe[];
+  
+  constructor(private recipeServices:RecipeService,
+    private router:Router,
+    private route:ActivatedRoute) {
+    
+   }
+  ngOnInit() {
+    this.recipes= this.recipeServices.getRecipes();
+  }
+  // onRecipeSelected(recipe:Recipe){
+  //   this.recipeWasSelected.emit(recipe);
+  // }
+  onNewRecipe(){
+    this.router.navigate(['new'],{relativeTo:this.route})
+    }
+
+}
